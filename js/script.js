d8a7ecdae5410dbb10d2102a66dc496b304d1089
joaovictorjulio <julio.guerra@tridia.com.br>
2023-07-09 23:34:30 -0300

    == Java Sript e CSS + Modularizando os scripts; + Adicionando modal; + Adicionando rolagem suave; + Adicionando animação para scroll;

---
+++ js/script.js
@@ -0,0 +1,12 @@
+import initAccordion from './modules/accordion.js';
+import initAnimacaoScroll from './modules/animacao_scroll.js';
+import initSmoothScroll from './modules/scroll_suave.js'
+import initTabNav from './modules/navtab.js';
+import initModal from './modules/modal.js';
+
+
+initSmoothScroll();
+initAnimacaoScroll();
+initAccordion();
+initTabNav();
+initModal();
