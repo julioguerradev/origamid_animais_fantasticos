cb8cccd48ffb469fddd93281b53a7bea779c4778
julioguerradev <julio.guerra.dev@gmail.com>
2023-07-13 01:26:49 -0300

    == Modulos de: + Dropdown Outside;

--- js/modules/dropdown_menu.js
+++ js/modules/dropdown_menu.js
@@ -22,5 +22,25 @@ function handleClick(event) {
     });
 }
 
+function outsideClick(element, events,callback) {
+    const html = document.documentElement;
+    const outside = 'data-outside'
 
+    if(!element.hasAttribute(outside)){
+        events.forEach(userEvent => {
+            html.addEventListener(userEvent, handleOusideClick);
+        })
+        element.setAttribute(outside, '')
+    }
+
+    function handleOusideClick(event) {
+        if(!element.contains(event.target)){
+            element.removeAttribute(outside)
+            events.forEach(userEvent => {
+                html.removeEventListener(userEvent, handleOusideClick);
+            })
+            callback()
+        }
+    }
+}
 
