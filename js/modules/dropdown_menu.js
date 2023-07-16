d3058005bace82daa921d9303a211507040d161f
julioguerradev <julio.guerra.dev@gmail.com>
2023-07-15 23:59:43 -0300

    == Java Script
    + Animando números;
    + Menu Mobile;
    + outsideClick como módulo;

--- js/modules/dropdown_menu.js
+++ js/modules/dropdown_menu.js
@@ -1,46 +1,23 @@
+import outsideClick from "./outsideclick.js"; 
+
 export default function initDropdownMenu() {
+    const dropdownMenus = document.querySelectorAll('[data-dropdown]');
 
-}
-
-const dropdownMenus = document.querySelectorAll('[data-dropdown]');
-
-dropdownMenus.forEach(menu => {
-    ['touchstart', 'click'].forEach(userEvent => {
-        menu.addEventListener(userEvent, handleClick)
-    })
-});
-
-
-
-
-
-function handleClick(event) {
-    event.preventDefault();
-    this.classList.add('active')
-    outsideClick(this, ['touchstart', 'click'], () =>{
-        this.classList.remove('active');
-    });
-}
-
-function outsideClick(element, events,callback) {
-    const html = document.documentElement;
-    const outside = 'data-outside'
-
-    if(!element.hasAttribute(outside)){
-        events.forEach(userEvent => {
-            html.addEventListener(userEvent, handleOusideClick);
+    dropdownMenus.forEach(menu => {
+        ['touchstart', 'click'].forEach(userEvent => {
+            menu.addEventListener(userEvent, handleClick)
         })
-        element.setAttribute(outside, '')
-    }
-
-    function handleOusideClick(event) {
-        if(!element.contains(event.target)){
-            element.removeAttribute(outside)
-            events.forEach(userEvent => {
-                html.removeEventListener(userEvent, handleOusideClick);
-            })
-            callback()
-        }
+    });
+    
+    function handleClick(event) {
+        event.preventDefault();
+        this.classList.add('active')
+        outsideClick(this, ['touchstart', 'click'], () =>{
+            this.classList.remove('active');
+        });
     }
 }
+
+
+
 
--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@ import initTabNav from './modules/navtab.js';
 import initModal from './modules/modal.js';
 import initTolltip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdown_menu.js';
+import initMenuMobile from './modules/menu_mobile.js';
+import initAnimaNumeros from './modules/anima_numeros.js';
 
 
 initSmoothScroll();
@@ -14,3 +16,5 @@ initTabNav();
 initModal();
 initTolltip();
 initDropdownMenu();
+initMenuMobile();
+initAnimaNumeros();
