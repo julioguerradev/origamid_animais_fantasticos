5cd170186f9dd76be7cd11822f112dcf5a4a4ceb
julioguerradev <julio.guerra.dev@gmail.com>
2023-07-16 15:24:01 -0300

    ScollSuave transformado em classe

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
-import initAccordion from './modules/accordion.js';
-import initAnimacaoScroll from './modules/animacao_scroll.js';
-import initSmoothScroll from './modules/scroll_suave.js'
-import initTabNav from './modules/navtab.js';
-import initModal from './modules/modal.js';
-import initTolltip from './modules/tooltip.js';
-import initDropdownMenu from './modules/dropdown_menu.js';
-import initMenuMobile from './modules/menu_mobile.js';
-import initFuncionamento from './modules/funcionamento.js';
-import initFetchAnimais from './modules/fetch_animais.js';
+import ScrollSuave from "./modules/scroll_suave.js";
+import initAccordion from "./modules/accordion.js";
+import initAnimacaoScroll from "./modules/animacao_scroll.js";
+import initTabNav from "./modules/navtab.js";
+import initModal from "./modules/modal.js";
+import initTolltip from "./modules/tooltip.js";
+import initDropdownMenu from "./modules/dropdown_menu.js";
+import initMenuMobile from "./modules/menu_mobile.js";
+import initFuncionamento from "./modules/funcionamento.js";
+import initFetchAnimais from "./modules/fetch_animais.js";
 
+const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
+scrollSuave.init();
 
-initSmoothScroll();
 initAnimacaoScroll();
 initAccordion();
 initTabNav();
