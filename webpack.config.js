5cbfb4274d324c968d2438ea41bf1ad8c1b89815
julioguerradev <julio.guerra.dev@gmail.com>
2023-07-22 23:47:54 -0300

    Adicionando Readme parcial Corrigindo caminho da api.sjon Alterando webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   entry: "./js/script.js",
   output: {
-    path: path.resolve(__dirname, "./js/"),
+    path: path.resolve(__dirname, "./"),
     filename: "main.js",
   },
   module: {
