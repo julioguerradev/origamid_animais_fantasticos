2fddea31048dc71550c6369c4cf7d8b315ec4767
julioguerradev <julio.guerra.dev@gmail.com>
2023-07-22 23:07:24 -0300

    bugfix accordion

--- js/modules/accordion.js
+++ js/modules/accordion.js
@@ -12,7 +12,7 @@ export default class Accordion {
   // Adiciona os eventos ao Accordion
   addAccordionEvent() {
     this.accordionList.forEach((item) => {
-      item.addEventListener("click", () => this.toggleAccordion);
+      item.addEventListener("click", () => this.toggleAccordion(item));
     });
   }
 
